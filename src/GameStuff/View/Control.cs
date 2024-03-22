430bb5bdf6e4f603052bf025aa31c39724475003
robertkar <kareckirobert@gmail.com>
2024-03-22 22:11:15 +0100

    Progress for controls for the UI

---
+++ src/GameStuff/View/Control.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100commitow.src.GameStuff.View
+{
+    public class Control
+    {
+
+    }
+}
